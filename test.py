c68311e715d7c33216f5d8b3a8b4626ff176bd79
Nishant Sahoo <nishantsahoo.in@gmail.com>
2019-11-08 01:47:35 +0530

    Update

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import time
-file_test = open('text.txt', w)
+file_test = open('text.txt', 'w')
 file_test.write('First')
+file_test.close()
 time.sleep(10)
+file_test = open('text.txt', 'w')
 file_test.write('Second')
 file_test.close()
